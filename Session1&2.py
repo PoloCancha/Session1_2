fbe2d4702e027c3606ca5bb8c9558582b77601e8
PoloCancha <pabscampana@gmail.com>
2026-01-20 16:59:16 +0100

    My first commit ever

---
+++ Session1&2.py
@@ -0,0 +1,21 @@
+# This is my first code
+# I am just learning Python
+print("Hello World")
+# print(6+7)
+print("6+7")
+print('And John Asked: "How are you?"')
+
+# String Parameter and Sum
+print("3 + 4 =", 3+4)
+print("3 + 4 =", 3+4, "and 5 x 6 =", 5*6)
+print("I have $", 100+200, ".")
+# How to get rid of unwanted Spaces
+print("I have $", 100+200, ".", sep="")
+# end is used to tell print what to do after it prints
+print("1", end="")
+print("2", end="")
+# backslash \ used to create bigger enters
+print("3", end="Bob stole my car\n\n")
+print("4", end="\n\n")
+print("5")
+
